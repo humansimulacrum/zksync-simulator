7b2c5cac4e9aae4f3c35df95ee93ae1d4fe05c43
Mykhailo Benych <misha.benych@gmail.com>
2024-01-06 19:55:26 +0100

    fixed sum retrieval

--- src/actions/balances.ts
+++ src/actions/balances.ts
@@ -25,18 +25,23 @@ export async function main() {
     let sum = 0;
 
     balance.forEach((record) => {
-      output[record.symbol] = record.valueInUsd;
+      output[record.symbol] = Number(record.valueInUsd.toFixed(2));
       sum += record.valueInUsd;
     });
 
-    return { ...output, Sum: sum };
+    output['ETH'] = output['ETH'] || 0;
+    output['WETH'] = output['WETH'] || 0;
+    output['USDC'] = output['USDC'] || 0;
+    output['WBTC'] = output['WBTC'] || 0;
+
+    return { ...output, Sum: Number(sum.toFixed(2)) };
   });
 
   const sortedData = sortBy(dataForTable, 'Sum').reverse();
   const totalSum = sumBy(sortedData, 'Sum');
 
-  console.table(sortedData, ['walletAddress', 'ETH', 'WETH', 'USDC', 'MUTE', 'WBTC', 'Sum']);
-  console.log(`Total sum in ZkSync => ${totalSum} USD.`);
+  console.table(sortedData, ['walletAddress', 'ETH', 'WETH', 'USDC', 'WBTC', 'Sum']);
+  console.log(`Total sum in ZkSync => ${totalSum.toFixed(2)} USD.`);
 
   process.exit(0);
 }
